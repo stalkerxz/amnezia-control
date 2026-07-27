19d02c214183955b68880d4861e6d477cbf6bdf8
stalkerxz <stalkerxz@bk.ru>
2026-07-27 20:57:48 +0500

    Disable server peer import route by default

--- amnezia_control/servers/urls.py
+++ amnezia_control/servers/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.urls import path
+
 from .views import (
     server_create_client_view,
     server_detail_view,
@@ -11,6 +13,8 @@ urlpatterns = [
     path("", server_list_view, name="servers-list"),
     path("<int:pk>/", server_detail_view, name="servers-detail"),
     path("<int:pk>/sync-runtime/", server_sync_runtime_view, name="servers-sync-runtime"),
-    path("<int:pk>/import-peers/", server_import_peers_view, name="servers-import-peers"),
     path("<int:pk>/create-client/<str:protocol_type>/", server_create_client_view, name="servers-create-client"),
 ]
+
+if settings.ENABLE_RUNTIME_PEER_IMPORT:
+    urlpatterns.append(path("<int:pk>/import-peers/", server_import_peers_view, name="servers-import-peers"))
