661ab100ec3d6713dff21432965056045527deed
PROTOPOPOV PRODUCTION <stalkerxz@bk.ru>
2026-08-21 01:42:43 +0500

    Install AWG agent backend at startup

--- amnezia_control/servers/apps.py
+++ amnezia_control/servers/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ServersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "servers"
+
+    def ready(self):
+        from .agent_backend import install_agent_backend
+
+        install_agent_backend()
